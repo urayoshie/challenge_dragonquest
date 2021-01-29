dc03a377fd62e3b06436a7c070e2d98eb4499764
Yoshie <yoshieura@icloud.com>
2021-01-29 18:24:43 +0900

    first_commit

---
+++ main.rb
@@ -0,0 +1,30 @@
+require './character'
+require './brave'
+require './monster'
+
+character = Character.new
+brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
+monster = Monster.new(name: "アークデーモン", hp: 210, offense: 140, defense: 80)
+monster = Monster.new(name: "シドー", hp: 250, offense: 255, defense: 255)
+
+
+
+monster.appear(monster)
+
+while brave.hp > 0 && monster.hp > 0 do
+
+  brave.attack(monster)
+  monster.attack(brave)
+  puts <<~TEXT
+        *=*=*=*=*=*=*=*=*=*=*=*=*=*
+        【#{brave.name}】 HP: #{brave.hp}
+        【#{monster.name}】 HP: #{monster.hp}
+        *=*=*=*=*=*=*=*=*=*=*=*=*=*
+      TEXT
+end
+
+if monster.hp == 0
+  puts "#{monster.name}をやっつけた"
+else brave.hp == 0
+  puts "#{brave.name}はしんでしまった"
+end
---
+++ monster.rb
@@ -0,0 +1,41 @@
+class Monster < Character
+
+  def appear(monster)
+    puts "#{monster.name}があらわれた！"
+  end
+
+  def attack(brave)
+
+    puts "#{@name}の攻撃！"
+
+    # モンスターからゆうしゃへのダメージの計算の呼び出し
+    damage = calculate_damage(brave)
+
+    # ゆうしゃへのダメージの呼び出し
+    damage_brave(brave, damage)
+
+  end
+
+  private
+
+  def calculate_damage(brave)
+
+    # モンスターから勇者へのダメージの計算
+    damage =  @offense - brave.defense / 2
+
+  end
+
+  # ゆうしゃへのダメージ
+  def damage_brave(brave, damage)
+
+    # ゆうしゃのHPから計算したダメージを引く（ゆうしゃの残りのHP）
+    brave.hp -= damage
+
+    # もしゆうしゃのHPがマイナスになるなら0を代入
+    brave.hp = 0 if brave.hp < 0
+
+    puts "#{brave.name}は#{damage}のダメージを受けた！"
+
+  end
+
+end
